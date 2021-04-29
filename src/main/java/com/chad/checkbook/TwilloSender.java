faf84d298f22cb21295f42690d1a3c28f201097e
cpick <cpickard2790@gmail.com>
2021-04-28 21:05:07 -0400

    Twilio SID and AUTH get value from application.properties

--- src/main/java/com/chad/checkbook/TwilloSender.java
+++ src/main/java/com/chad/checkbook/TwilloSender.java
@@ -3,12 +3,20 @@ package com.chad.checkbook;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
 
 public class TwilloSender {
+
+    @Value("${app.twilio_sid}")
+    private static String twilioSID;
+
+    @Value("${app.twilio_auth}")
+    private static String twilioAUTH;
+
     // Find your Account Sid and Token at twilio.com/console
     // and set the environment variables. See http://twil.io/secure
-    public static final String ACCOUNT_SID = "AC96ba09abd5d5cf9626296c19787b9cfd";
-    public static final String AUTH_TOKEN = "df61fd6f00f7d2eb8efdc619e48d07e8";
+    public static final String ACCOUNT_SID = twilioSID;
+    public static final String AUTH_TOKEN = twilioAUTH;
 
     public static void sendMessage(double amount) {
     	try {
